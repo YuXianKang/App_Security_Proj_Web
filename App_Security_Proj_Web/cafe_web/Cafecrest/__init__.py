0dd782d90a2ecf223ebf3d030a1af160de6804a5
Kang Yu Xian <152857503+YuXianKang@users.noreply.github.com>
2024-08-07 22:34:17 +0800

    Update the __init__.py file

--- App_Security_Proj_Web/cafe_web/Cafecrest/__init__.py
+++ App_Security_Proj_Web/cafe_web/Cafecrest/__init__.py
@@ -7,7 +7,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from sqlalchemy.exc import IntegrityError
 from markupsafe import escape
-from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from datetime import timedelta, datetime
@@ -30,7 +29,6 @@ app = Flask(__name__)
 app.config.from_object(config)
 app.register_blueprint(errors_bp)
 
-CORS(app)
 limiter = Limiter(key_func=get_remote_address, app=app)
 
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
